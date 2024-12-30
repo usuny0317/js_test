673de4dbcc2b8e567ee0780a1d050c497f89959d
usuny0317 <usuny0317@naver.com>
2024-12-30 10:13:31 +0900

    각도기 설명 추가 어려움!

--- 241230각도기.js
+++ 241230각도기.js
@@ -7,3 +7,11 @@ function solution(angle) {
     else{answer=-1}
     return answer;
 }
+//더 나은 방법
+// anglr<90 ? 1: angle == 90 ? 2 : angle <180 ? 3 : 4
+//조건에 정수, 180이하 0초과라서 가능한
+
+//이해는 안가지만 더 깔끔해 보이는 방법
+//return [0, 90, 91, 180].filter(x => angle>=x).length;
+// angle이 기준 값들보다 크거나 같은 개수를 반환
+//ex) >> 배열 [0, 90, 91, 180]에서 91보다 크거나 같은 값: 0, 90, 91. 결과: [0, 90, 91] → 길이: 3
