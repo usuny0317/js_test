68c4fd47d3027f64fad8fec7a647bf154a74eb23
usuny0317 <usuny0317@naver.com>
2024-12-30 09:57:23 +0900

    각도기

---
+++ 241230각도기.js
@@ -0,0 +1,9 @@
+function solution(angle) {
+    var answer = 0;
+    if(angle<90 && angle>0){answer=1}
+    else if (angle==90){answer=2}
+    else if (angle>90&& angle<180){answer=3}
+    else if (angle==180){answer=4}
+    else{answer=-1}
+    return answer;
+}
